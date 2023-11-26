96a8f0a455e3bb5bd2a9195a7313a68589635458
Arman Ommid <armanommid@gmail.com>
2023-11-26 10:19:38 -0800

    push

--- runners/experiment.py
+++ runners/experiment.py
@@ -1,40 +1,6 @@
 
 import torch
 
-# Seperated into another function to collect memory usage data if needed
-def run_experiment(model, optimizer, criterion, dataloader, epochs, device, SAVE_WEIGHTS_FILE):
-    losses = []
-    dataset_size = len(dataloader.dataset)
-    for e in range(epochs):
-        print("===="*4, f"Epoch {e}", "===="*4)
-
-        epoch_loss = 0
-        for iter, (inputs, labels) in enumerate(dataloader):
-            inputs, labels = inputs.to(device), labels.to(device)
-            
-            model.train(True)
-            optimizer.zero_grad()
-            with torch.enable_grad():
-                outputs = model(inputs)
-                loss = criterion(outputs, labels)
-                loss.backward()
-                optimizer.step()
-            model.train(False)
-
-            avg_batch_loss = loss.item()
-            epoch_loss += avg_batch_loss * inputs.size(0)
-            print(f"BatchAvgLoss: {avg_batch_loss}")
-
-        losses.append(epoch_loss / dataset_size)
-        print("----"*16)
-        print(f"Epoch: {e}")
-        print(f"EpochAvgLoss: {losses[e]}")
-        print(SAVE_WEIGHTS_FILE)
-        model.save(SAVE_WEIGHTS_FILE)
-
-    print("===="*4, "Done", "===="*4)
-    print(SAVE_WEIGHTS_FILE)
-
 def run_epoch(
     model,
     criterion,
