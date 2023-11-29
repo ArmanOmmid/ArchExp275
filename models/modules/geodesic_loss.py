42e90c772dc6147f06b786ece6e8f455a5456940
Arman Ommid <armanommid@gmail.com>
2023-11-29 10:07:05 -0800

    progress

--- models/modules/geodesic_loss.py
+++ models/modules/geodesic_loss.py
@@ -12,7 +12,7 @@ class SpecialEuclideanGeodesicLoss(_Loss):
 
     def normalize(self, rot_matrix):
         u, s, v = torch.svd(rot_matrix)
-        return torch.bmm(u, v.transpose(1, 2))
+        return torch.bmm(u, v.transpose(-2, -1))
 
     def forward(self, predicted_transform, target_transform):
         # Transforms are 3x4 with a 3x3 in SO(3) and a 3x1 in R(3)
@@ -28,11 +28,11 @@ class SpecialEuclideanGeodesicLoss(_Loss):
         p_R = self.normalize(p_R)
         t_R = self.normalize(t_R)
 
-        relative_rotation = torch.bmm(p_R, t_R.transpose(1, 2))
+        relative_rotation = torch.bmm(p_R, t_R.transpose(-2, -1))
 
-        batch_trace = torch.diagonal(relative_rotation, dim1=1, dim2=2).sum(dim=1)
+        batch_trace = torch.diagonal(relative_rotation, dim1=-2, dim2=-1).sum(dim=-1)
 
-        cos_theta = (batch_trace - 1) / 2
+        cos_theta = (batch_trace - 1.0) / 2.0
         cos_theta = torch.clamp(cos_theta, -0.99, 0.99)  # Numerical stability
         theta = torch.acos(cos_theta)
 
