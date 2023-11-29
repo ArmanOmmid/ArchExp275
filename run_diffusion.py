7e13608f4d9fb0611535001de042afdb1cca3ff1
Arman Ommid <armanommid@gmail.com>
2023-11-29 14:54:49 -0800

    progress

--- run_diffusion.py
+++ run_diffusion.py
@@ -139,7 +139,7 @@ def main(args):
                            output_latent_dims=output_latent_dims, 
                            )
 
-    summary(model)
+    summary(model, input_size=[1, input_latent_dims, IMG_H, IMG_W])
     # print(model)
     # print(args)
     
