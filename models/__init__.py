564e7aee51303f3a41d70ce526a5b904dd3749ae
Arman Ommid <armanommid@gmail.com>
2023-11-29 22:50:29 -0800

    progress

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from .unet import UNet, UNet0
 from .xswin import XNetSwinTransformer
 from .xswin_diffusion import XNetSwinTransformerDiffusion
 from .pointnet import PointNet
+from .posenet import PoseNet
