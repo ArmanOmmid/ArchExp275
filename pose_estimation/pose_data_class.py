d5bbda63a1cf03cf31e78f45a33882a2cab626e0
Arman Ommid <armanommid@gmail.com>
2023-11-30 10:00:39 -0800

    progress

--- pose_estimation/pose_data_class.py
+++ pose_estimation/pose_data_class.py
@@ -222,14 +222,17 @@ class PoseData:
 
         NUM_CLASSES = len(pose_data.objects)
         object_infos = [None] * NUM_CLASSES
-        source_meshes = [None] * NUM_CLASSES
+
+        mesh_path = os.path.join(dataset_folder, "meshes")
+        os.makedirs(mesh_path)
         for i in range(NUM_CLASSES):
             object_infos[i] = pose_data.get_info(i)
-            source_meshes[i] = pose_data.get_mesh(i)
+            mesh_path_i = os.path.join(mesh_path, f'{i}.obj')
 
-        np.savez(os.path.join(dataset_folder, f"objects.npz"), 
-                    infos=object_infos, 
-                    meshes=source_meshes)
+            mesh_i = pose_data.get_mesh(i)
+            mesh_i.export(file_obj=mesh_path_i, file_type='obj')
+
+        np.save(os.path.join(dataset_folder, f"objects.npy"),  np.array(object_infos))
         
         scene_data = os.path.join(dataset_folder, "scenes")
         os.makedirs(dataset_folder)
